4f4a03ed4ad78e04f8ea6db04cec6e6980471dfe
Tiya Gallegos <tiyag@Tiyas-MacBook-Pro.local>
2020-05-09 16:47:53 -0600

    STYLE CHANGES

--- js/script.js
+++ js/script.js
@@ -17,7 +17,15 @@ const $albumCover = $("#album-cover")
 const $audio = $("#audio")
 /*----- event listeners -----*/
 
-//.on('click', "form", handleGetData);
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  //$('.dropdown-trigger').dropdown();
+        
 
 /*----- functions -----*/
 function settings(url) {
